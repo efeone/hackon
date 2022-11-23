d806b62aa3141ef58dfa844c4f8a6cd7df85e9dc
Anjana-sajeevan <anjanasajeevs@gmail.com>
2022-11-23 15:09:10 +0530

    feat: Created Tool Master and Tools used and the purpose need to be noted in Template, Task

---
+++ hackon/hackon/doctype/software_tool/software_tool.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, efeone and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Software Tool', {
+	// refresh: function(frm) {
+
+	// }
+});
