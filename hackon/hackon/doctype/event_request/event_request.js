c9bc8c971eb9fbe41ce4d383c88824063b2dd592
Keerthana-ck <keerthana98323@gmail.com>
2022-11-28 13:03:55 +0530

    feat:Create Event Request Doctype and add Basic Event Details

---
+++ hackon/hackon/doctype/event_request/event_request.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, efeone and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Event Request', {
+	// refresh: function(frm) {
+
+	// }
+});
