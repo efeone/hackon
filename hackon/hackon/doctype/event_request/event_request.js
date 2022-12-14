1af6ce1b6883a30f9331f4b9d3d8a8585f73680a
aswani-m <m.aswanibabu@efeone.com>
2022-12-14 15:46:14 +0530

    chor: Edit field in Event request doctype

--- hackon/hackon/doctype/event_request/event_request.js
+++ hackon/hackon/doctype/event_request/event_request.js
@@ -2,7 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Event Request', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+    frappe.db.get_single_value('Hackon Settings', 'maximum_allowed_team').then( maximum_allowed_team=>{
+      frm.set_value('maximum_allowed_team', maximum_allowed_team);
+    });
+	}
 });
