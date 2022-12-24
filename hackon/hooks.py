4fe9a88e0bd6610c188d3f13d49fe8b9265dc58d
SherinKR <sherinkrply@gmail.com>
2022-12-24 17:04:05 +0530

    fix: Roles and Permissions

--- hackon/hooks.py
+++ hackon/hooks.py
@@ -195,7 +195,7 @@ user_data_fields = [
 
 fixtures = [
 		{"dt": "Role","filters": [["name", "in", ['Participant', 'Super Admin', 'Judge','Team Lead', 'Mentor', 'Host Organizer', 'Event Requester']]]},
-		{"dt": "Custom DocPerm","filters": [["role", "in", ['Participant', 'Super Admin', 'Judge','Team Lead', 'Mentor', 'Host Organizer', 'Event Requester']]]},
+		{"dt": "Custom DocPerm","filters": [["parent", "in", ['Task', 'Project', 'Event', 'Project Template']]]},
 		{"dt": "Workflow State", "filters": [["name", "in", ["Approved by Mentor", "Draft", "Approved by Judges", "Completed","Draft","Pending review","Approved","Rejected"]]]},
 		{"dt": "Workflow", "filters": [["name", "in", ["Task Workflow","Event Request Workflow"]]]},
 		{"dt": "Energy Point Rule", "filters": [["name", "in", ["On Task Completion"]]]},
